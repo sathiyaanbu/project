a35b95ffb04dbd3d84276bd988090d153186dedf
sathiyaanbu <msnature12@gmail.com>
2016-11-14 15:15:06 +0200

    koira class

---
+++ koira.java
@@ -0,0 +1,51 @@
+package testiluokka;
+
+public class koira {
+	String nimi;
+	kissa jahtajaa;
+	
+	
+	
+	public String toString() {
+		return "Koira [nimi=" + "]";
+	}}
+		
+		
+
+/*	public void find(String a){
+	System.out.println("hello matti!"+a);
+}
+
+
+}
+*/
+
+
+	
+		
+	
+		
+	
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
