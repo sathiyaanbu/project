1d0716ad2002223ccf666fa14fb380b054e4adc2
sathiyaanbu <msnature12@gmail.com>
2016-11-14 13:42:09 +0200

    java program

---
+++ world.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package hei;
+
+
+public class world {
+
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+		int a =2,b =5;
+		int sum = a+b;
+		System.out.println("sum =" + sum);
+		
+		
+	}
+
+}
+
