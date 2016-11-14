dd9fe2535da55b3a2642339d88b5eb08ec443bbc
sathiyaanbu <msnature12@gmail.com>
2016-11-14 15:14:27 +0200

    kissa class

---
+++ kissa.java
@@ -0,0 +1,25 @@
+package fi.agileo.test;
+
+
+
+public class kissa {
+	
+
+		String nimi;
+		kissa jahtaaja;
+		public void karkuun() {
+			
+		}
+public void karkuun(kissa koira) {
+			
+			
+		}
+		public String toString() {
+			return "Kissa [nimi=" + "]";
+				
+		}
+
+		
+
+	}
+
